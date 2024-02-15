dce5f41739cbf8814193c92312fe4f08a0bdadda
Thayná de Matos Sousa <thaynamatos956@gmail.com>
2024-02-15 10:07:44 -0300

    Initial commit

---
+++ 01/app.py
@@ -0,0 +1,7 @@
+idade = int(input("insira a sua idade:"))
+
+if idade >= 18:
+    print("idade maior que 18 anos")
+else:
+    print("idade menor que 18 anos")    
+print("fim")
---
+++ README.md
@@ -0,0 +1,2 @@
+# aprendendo-pyhton
+ primeiros passos em python
