f6a3ba4a639d0da102586f7c561fa45fb6f6b357
Thayná de Matos Sousa <thaynamatos956@gmail.com>
2024-02-16 09:53:17 -0300

    02

---
+++ 02/app.py
@@ -0,0 +1,11 @@
+nota1 = 8
+nota2 = 6
+soma = nota1 + nota2
+
+if(nota2>nota1):
+    print("nota 2 eh maior que nota 1")
+    nota1=1
+else:
+    print("nota 2 nao eh maior que nota 1")
+    if((soma/2 > nota2/2)):
+        print(nota2)
---
+++ 03/app.py
@@ -0,0 +1,9 @@
+x = 10
+exp = x**2
+
+if(exp >= (x+200)):
+    print(exp)
+    print("o valor de exp eh maior ou igual a x")
+else:
+    print(exp)
+    print("o valor de exp eh menor que x")
