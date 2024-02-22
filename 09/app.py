fffe53281f67384e68d9628ea5cdd7b9c3a3cb3c
Thayná de Matos Sousa <thaynamatos956@gmail.com>
2024-02-22 18:57:49 -0300

    calculadora

--- 09/app.py
+++ 09/app.py
@@ -1,1 +1,10 @@
- 
+num1 = input('insira um numero:')
+num2 = input('insira outro numero:')
+
+if num1.isdigit() and num2.isdigit():
+    num1 = int(num1)
+    num2 = int(num2)
+
+    print(num1 + num2)
+else:
+    print('nao pude converter os numeros para realizar a conta')
