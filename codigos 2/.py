320f2c175db911f479c75770fca253f1b5adcfb1
Thayná de Matos Sousa <thaynamatos956@gmail.com>
2024-03-05 10:21:18 -0300

    codigos

---
+++ codigos 2/.py
@@ -0,0 +1,8 @@
+#nomes = 'thayna', 'thiago', 'gabriel'
+#for nome in nomes:
+#    print(nome)
+
+#valor_max = int(input('Digite um numero:'))
+#valor_inicial = 1
+#for numero in range(valor_inicial, valor_max + 1):
+#    print(numero)
---
+++ codigos/app.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 = 3
+divisao = num1/num2
+nome = 'thayna'
+#print('{:.2f}'.format(divisao))
+#print(f'{num1:0>10}')
+#print(f'{nome:#^10}')
+#nome_formatado = '{n}'.format(n=nome)
+#print(nome_formatado)
+
+################################
+
+texto = 'python'
+print(texto[3])
