593a9945556a905d4e40c0ae66c9ae1b601f9662
Thayná de Matos Sousa <thaynamatos956@gmail.com>
2024-03-06 11:59:38 -0300

    16

---
+++ 16/.py
@@ -0,0 +1,20 @@
+'''
+contador = 1
+acumulador = 1
+while contador <= 10 :
+    print(contador, acumulador)
+
+    acumulador = acumulador + contador
+    contador += 1
+else:
+    print('cheguei no else.')
+'''
+'''
+frase = 'o rato roeu a roupa do rei de roma'
+tamanho_frase = len(frase)
+contador = 0
+
+while contador < 10:
+    print(frase[contador], contador)
+    contador += 1 
+'''
