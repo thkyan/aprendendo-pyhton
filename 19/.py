08a869d1ac262cd9e9023f8f3d61804ed1b4ca04
Thayná de Matos Sousa <thaynamatos956@gmail.com>
2024-03-19 09:54:34 -0300

    19

---
+++ 19/.py
@@ -0,0 +1,30 @@
+# def operacoes_basicas(num1, num2):
+#     soma = num1 + num2
+#     diferença = num1 - num2
+#     mult = num1 * num2
+#     divisao = num1 / num2
+#     return (soma, diferença, mult, divisao)
+
+# print (operacoes_basicas(10,2))
+
+# meta = 10000
+# vendas = {
+#     'joao':15000,
+#     'julia':27000,
+#     'marcus':9900,
+#     'maria':3750,
+#     'ana':10300,
+#     'alon':7870,
+# }
+
+# def calculo_meta(meta, vendas):
+#     bateram_meta = []
+#     for vendendor in vendas:
+#         if vendas[vendendor] >= meta:
+#             bateram_meta.append(vendendor)
+
+#     perc_baeterammeta = len(bateram_meta)/len(vendas)
+#     return perc_baeterammeta, bateram_meta
+
+# print(calculo_meta(meta, vendas))
+
