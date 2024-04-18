b0d4c359b5999081e3b375a7dd62ab88b1af9137
Thayná de Matos Sousa <thaynamatos956@gmail.com>
2024-04-18 09:10:49 -0300

    up

--- 23/.py
+++ 23/.py
@@ -25,3 +25,11 @@
 
 ###################################################################
 
+# lista = [
+#     ('chave', 'valor'),
+#     ('chave2', 'valor2'),
+# ]
+
+# d1 = {x:y for x, y in enumerate(range(5))}
+d1 = {f'chave_{x}': x**2 for x in range(5)}
+print(d1)
