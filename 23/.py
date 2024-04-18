aefb38e46899ccae744ceec8363cad53094a124a
Thayná de Matos Sousa <thaynamatos956@gmail.com>
2024-04-18 09:01:31 -0300

    23

--- 23/.py
+++ 23/.py
@@ -15,10 +15,13 @@
 
 # print(ex5)
 
-string = '0123456789012345678901234567890123456789'
-n = 10
-lista = [string[i:i+n] for i in range(0, len(string), n)]
-print(lista)
+# string = '0123456789012345678901234567890123456789'
+# n = 10
+# lista = [string[i:i+n] for i in range(0, len(string), n)]
+# print(lista)
 
-retorno = '.'.join(lista)
-print(retorno)
+# retorno = '.'.join(lista)
+# print(retorno)
+
+###################################################################
+
