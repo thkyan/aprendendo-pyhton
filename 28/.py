0cd5017e19344b4295d1143d0ea8142c4bc0b9e1
Thayná de Matos Sousa <thaynamatos956@gmail.com>
2024-04-30 10:32:34 -0300

    up

--- 28/.py
+++ 28/.py
@@ -1,9 +1,14 @@
 from itertools import count
 
-contador = count(start=5, step=-1)
+# contador = count(start=5, step=-1)
 
-for valor in contador:
-    print(round(valor, 2))
+# for valor in contador:
+#     print(round(valor, 2))
 
-    if valor >= 10 or valor <= -10:
-        break
+#     if valor >= 10 or valor <= -10:
+#         break
+
+contador = count()
+lista = ['thayna','thiago','lorrany']
+lista = zip(contador, lista)
+print(list(lista))
