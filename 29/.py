27af729db19aa520e84be3306612a33cb15fc069
Thayná de Matos Sousa <thaynamatos956@gmail.com>
2024-05-10 16:37:10 -0300

    classs

--- 29/.py
+++ 29/.py
@@ -1,11 +1,18 @@
-from itertools import combinations, permutations, product
+# from itertools import combinations, permutations, product
 
-pessoas = ['thayna', 'thiago', 'lorrany', 'larice']
-# for grupo in combinations(pessoas, 2):
+# pessoas = ['thayna', 'thiago', 'lorrany', 'larice']
+# # for grupo in combinations(pessoas, 2):
+# #     print(grupo)
+
+# # for grupo in permutations(pessoas):
+# #     print(grupo)
+
+# for grupo in product(pessoas, repeat=2):
 #     print(grupo)
 
-# for grupo in permutations(pessoas):
-#     print(grupo)
-
-for grupo in product(pessoas, repeat=2):
-    print(grupo)
+def calcular(*args):
+    r = sum(1, 4, 5)
+    for i in args:
+        r += i
+    return r
+print(calcular)
---
+++ 30/main.py
@@ -0,0 +1,5 @@
+from pessoa import Pessoa
+
+p1 = Pessoa('thayna', 18)
+p2 = Pessoa('thiago', 17)
+
